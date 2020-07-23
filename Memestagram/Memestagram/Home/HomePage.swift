521c2bcc1d60077f40b2e6e2a923cd4e4054e117
Niraj Tiwari <tniraj7@gmail.com>
2020-07-24 03:39:30 +0900

    design the home page

--- Memestagram/Memestagram/Home/HomePage.swift
+++ Memestagram/Memestagram/Home/HomePage.swift
@@ -5,6 +5,7 @@ struct HomeView: View {
         List {
             ForEach(0..<3, id: \.self) { num in
                 PostCell(i: num)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
             }
         }
     }
@@ -12,6 +13,6 @@ struct HomeView: View {
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        HomeView().previewDevice("iPhone 11")
     }
 }
--- Memestagram/Memestagram/Home/PostCell.swift
+++ Memestagram/Memestagram/Home/PostCell.swift
@@ -5,13 +5,45 @@ struct PostCell: View {
     
     var body: some View {
         VStack {
-            Text("\(i)")
+            VStack {
+                Image("boats")
+                    .resizable()
+                    .frame(height: 600, alignment: .center)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 10, x: 2, y: 2)
+                
+                HStack {
+                    Image("boats")
+                        .resizable()
+                        .frame(width: 50, height: 50, alignment: .center)
+                        .cornerRadius(25)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Build That App")
+                        
+                        Text("1 hr ago")
+                            .font(.caption)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
+                    }
+                    
+                    Spacer()
+                }.padding()
+                
+                Divider().padding(.horizontal)
+                
+                Text("eafeajfhbeafeahbfhaebfheab hjaebfjhbeafbhebfjaeb  feahjbfebjaef")
+                    .lineLimit(nil)
+                    .padding()
+            }
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(radius: 10)
+            .padding()
         }
     }
 }
 
 struct PostCell_Previews: PreviewProvider {
     static var previews: some View {
-        PostCell(i: 1)
+        PostCell(i: 1).previewDevice("iPhone 11")
     }
 }

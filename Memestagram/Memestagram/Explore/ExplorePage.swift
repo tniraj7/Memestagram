2e98d8ff9614284935380e2db066dac6bdcfada9
Niraj Tiwari <tniraj7@gmail.com>
2020-07-24 04:11:29 +0900

    design Explore page

--- Memestagram/Memestagram/Explore/ExplorePage.swift
+++ Memestagram/Memestagram/Explore/ExplorePage.swift
@@ -1,13 +1,96 @@
 import SwiftUI
 
-struct ExplorePage: View {
+struct ExploreView: View {
+    
+    @State var searchText = String()
+    @State var isSearching: Bool = false
+    
+    var exampleData = [
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+        PostIdentifiable(),PostIdentifiable(),
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            
+            VStack {
+                
+                TextField("Enter a keyword", text: self.$searchText, onEditingChanged: { changed in
+                    
+                }, onCommit: {
+                    self.search()
+                })
+                .padding()
+                
+                
+                if isSearching {
+                    List {
+                        ForEach(0..<3, id: \.self) { num in
+                            PostCell(i: num)
+                                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        }
+                    }
+                } else {
+                    QGrid(exampleData, columns: 3, columnsInLandscape: nil, vSpacing: 0, hSpacing: 0, vPadding: 0, hPadding: 0, isScrollable: true, showScrollIndicators: false, content: { post in
+                        
+                        post.image
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: UIScreen.main.bounds.width / 3,
+                                   height: UIScreen.main.bounds.height / 3,
+                                   alignment: .center)
+                            .clipped()
+                    })
+                }
+            }
+            .navigationBarTitle("Explore", displayMode: .inline)
+        }
+    }
+    
+    func search() {
+        if self.searchText == "" {
+            self.isSearching = false
+        } else {
+             self.isSearching = true
+        }
     }
 }
 
-struct ExplorePage_Previews: PreviewProvider {
+struct PostIdentifiable: Identifiable {
+    var id = UUID()
+    var image = Image("boats").resizable()
+                
+}
+
+struct ExploreView_Previews: PreviewProvider {
     static var previews: some View {
-        ExplorePage()
+        ExploreView().previewDevice("iPhone 11")
     }
 }

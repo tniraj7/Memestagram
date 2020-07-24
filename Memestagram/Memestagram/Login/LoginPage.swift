1f790b404d400ffd88a7a6bbb6ff461427f85a96
Niraj Tiwari <tniraj7@gmail.com>
2020-07-24 19:37:13 +0900

    design Login Page

---
+++ Memestagram/Memestagram/Login/LoginPage.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var isPresented: Bool = false
+    
+    var body: some View {
+        
+        VStack {
+            
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .leading,
+                    endPoint: .trailing)
+                    .frame(height: 200, alignment: .top)
+                    .edgesIgnoringSafeArea(.top)
+                    .shadow(radius: 15)
+                
+                Text("Memestagram")
+                    .foregroundColor(.white)
+                    .bold()
+                    .font(.largeTitle)
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            }
+            
+            TextField("Email", text: self.$email).padding()
+            SecureField("Password", text: self.$password).padding()
+            HStack {
+                Text("Don't have an account ?")
+                Button(action: self.signUp, label: { Text(" Sing up now!") })
+            }
+            Spacer()
+        }
+    }
+    
+    func signUp() { self.isPresented.toggle() }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}

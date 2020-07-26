15b1c062377693c03b3d35be0dbd630f1f355d0c
Niraj Tiwari <tniraj7@gmail.com>
2020-07-26 23:50:19 +0900

    add date field to Post realm object

--- Memestagram/Memestagram/RealmObject.swift
+++ Memestagram/Memestagram/RealmObject.swift
@@ -1,9 +1,31 @@
-//
-//  RealmObject.swift
-//  Memestagram
-//
-//  Created by Tiwari, Niraj | Nero | LPD on 2020/07/26.
-//  Copyright © 2020 NT. All rights reserved.
-//
-
 import Foundation
+import RealmSwift
+
+class Post: Object {
+    
+    @objc dynamic var id: String = ""
+    @objc dynamic var imageUrl: String = ""
+    @objc dynamic var comment: String = ""
+    @objc dynamic var user: UserObject? = nil
+    @objc dynamic var aspectRatio: Double = 0
+    @objc dynamic var date: Date? = nil
+    var isFavorited = RealOptional<Bool>()
+    
+    override class func primaryKey() -> String {
+        return "id"
+    }
+}
+
+class UserObject: Object {
+    
+    @objc dynamic var id: String = ""
+    @objc dynamic var username: String = ""
+    @objc dynamic var profileImageUrl: String = ""
+    
+    var isLoggedIn = RealOptional<Bool>()
+    var isFollowing = RealOptional<Bool>()
+    
+    override class func primaryKey() -> String {
+        return "id"
+    }
+}

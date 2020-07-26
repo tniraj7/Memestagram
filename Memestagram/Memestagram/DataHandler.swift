cfbfcf8308220f2ec909bbf319dd650a099adf9d
Niraj Tiwari <tniraj7@gmail.com>
2020-07-26 23:49:06 +0900

    remove unreferenced file

---
+++ Memestagram/Memestagram/DataHandler.swift
@@ -0,0 +1,9 @@
+//
+//  DataHandler.swift
+//  Memestagram
+//
+//  Created by Tiwari, Niraj | Nero | LPD on 2020/07/26.
+//  Copyright © 2020 NT. All rights reserved.
+//
+
+import Foundation
---
+++ Memestagram/Memestagram/RealmObject.swift
@@ -0,0 +1,9 @@
+//
+//  RealmObject.swift
+//  Memestagram
+//
+//  Created by Tiwari, Niraj | Nero | LPD on 2020/07/26.
+//  Copyright © 2020 NT. All rights reserved.
+//
+
+import Foundation

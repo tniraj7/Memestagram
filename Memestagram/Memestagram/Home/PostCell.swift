2fe2229ee6eddf3e215751b3ce099e0747a930b1
Niraj Tiwari <tniraj7@gmail.com>
2020-08-02 18:04:10 +0900

    remove extra placeholder

--- Memestagram/Memestagram/Home/PostCell.swift
+++ Memestagram/Memestagram/Home/PostCell.swift
@@ -15,7 +15,6 @@ struct PostCell: View {
                                 
                             }
                             .resizable()
-                            .placeholder(UIImage(systemName: "photo"))
                             .placeholder {
                                 Rectangle().foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
                             }
